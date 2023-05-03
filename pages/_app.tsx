ed101533aaae00e3c6a8ef4901715f184ee7fd70
CagatayUmt <cagataykumar18@gmail.com>
2023-05-03 14:36:51 +0300

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@babel/runtime / @babel/runtime-corejs3styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
