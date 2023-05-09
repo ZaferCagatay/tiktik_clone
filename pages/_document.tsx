de532cf42dfa14d3232b0d73cbb39b600a5fe4ea
CagatayUmt <cagataykumar18@gmail.com>
2023-05-09 20:12:49 +0300

    ...

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="https://www.linkpicture.com/q/tiktik-logo.ico" />
+        <title>TikTik</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
